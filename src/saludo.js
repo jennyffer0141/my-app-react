4035fb25ac577811a9f6c4719e61d92525f61616
jennyffer0141 <jennyffer0141@gmail.com>
2020-07-29 16:55:20 -0500

    mi primera publicacion

---
+++ src/saludo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const saludouno = (msj) => {
+    return ( 
+        <div>
+            <h1> JENNYFFER JHORDYT ...</h1>
+            
+        </div>
+     );
+}
+ 
+export default saludouno;
