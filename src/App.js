f1b6a1732e7c54f074f668599ff567353a5431f2
jennyffer0141 <jennyffer0141@gmail.com>
2020-08-21 08:35:40 -0500

    actualizacion examen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-
   return (
-    <div>
-      <h1>DISEÑO WEB :)</h1>
-    </div>
+    <div className="App">
+      <div id="cont">
+      <header id="menu">
+          <h1>CURRICULUM VITAE</h1>
+        </header>
+          
+          <section id="conte">
 
+          <a><article>
+             <p>JENNYFFER JHORDYT FERATA MAQQUE <br/>DNI 73181846 <br/>18 Años, Soltera<br/>Asoc. Ciudad de dios km.16 cmt. 22 mz.b lt.17 <br/>Yura-Arequipa<br/>962215504 (claro)<br/>E-mail jennyffer0141@gmail.com<br/></p>           
+            </article></a>
+            <article>
+            <p><br/>PERFIL PROFESIONAL:</p>
+            <p><br/>FORMACION ACADEMICA:</p>
+            <p><br/>EXPERIENCIA PROFESIONAL:</p>
+            <p><br/>IDIOMAS:</p>
+            <p><br/>INFORMATICA:</p>       
+            </article>
+            
+          </section>
+
+          <footer id="links">
+            <h1>Links</h1>
+            
+            <p>MOODLE</p>
+            <a href="https://unidadweb.milaulas.com/"> Ir a mi pagina web moodle</a>
+
+            <p>WORDPRESS</p>
+            <a href="http://www.sitioweb.epizy.com/"> ir a mi pagina web wordpress</a>
+            
+           
+          </footer>        
+      </div>
+    </div>
   );
 }
 
